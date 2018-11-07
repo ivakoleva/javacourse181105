f86d6ee887bb9b65ba35848fdae5da3e54049e97
Iva Koleva <iva.koleva@clearcode.bg>
2018-11-07 15:54:31 +0200

    date-time API, bridge to previous API

--- src/main/java/bg/clearcode/javacourse181105/ivakoleva/Person.java
+++ src/main/java/bg/clearcode/javacourse181105/ivakoleva/Person.java
@@ -21,6 +21,22 @@ public class Person implements Ageable {
         this.age = age;
     }
 
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
     public Person(int age) {
         this.age = age;
     }
