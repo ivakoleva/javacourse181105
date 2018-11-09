270319f249cd8b53c1571fe9afd5768032797bee
Iva Koleva <iva.koleva@clearcode.bg>
2018-11-09 10:03:31 +0200

    serialize & deserialize multiple objects, same file

--- src/main/java/bg/clearcode/javacourse181105/day4/Person.java
+++ src/main/java/bg/clearcode/javacourse181105/day4/Person.java
@@ -7,6 +7,7 @@ import java.io.Serializable;
  * a significant bit of leva.bg project.
  */
 public class Person implements Serializable {
+    //private static final long serialVersionUID = -3145898787727225331L;
     private static final Person INSTANCE = new Person();
     private String name;
     private int age;
@@ -33,4 +34,37 @@ public class Person implements Serializable {
     public void setAge(int age) {
         this.age = age;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (getAge() != person.getAge()) return false;
+        return getName() != null ? getName().equals(person.getName()) : person.getName() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getName() != null ? getName().hashCode() : 0;
+        result = 31 * result + getAge();
+        return result;
+    }
+
+    /*@Override
+    public Object clone() throws CloneNotSupportedException {
+        //return super.clone();
+        final Person person = new Person();
+        person.setName(this.getName());
+        person.setAge(this.getAge());
+        return person;
+    }*/
+
+    /*private void readObject(final ObjectInputStream s) throws IOException, ClassNotFoundException {
+        final Person personRead = (Person) s.readObject();
+        this.setName(personRead.getName());
+        this.setAge(personRead.getAge());
+    }*/
 }
--- src/main/java/bg/clearcode/javacourse181105/day4/SerializationRunner.java
+++ src/main/java/bg/clearcode/javacourse181105/day4/SerializationRunner.java
@@ -8,28 +8,41 @@ import java.io.*;
  */
 public class SerializationRunner {
     public static void main(final String[] args) {
-        final Person person = Person.getInstance();
-        person.setName("Georgi");
-        person.setAge(1);
+        final Person person1_0 = new Person();
+        person1_0.setName("Georgi");
+        person1_0.setAge(1);
+        final Person person1_1 = new Person();
+        person1_1.setName("Valio");
+        person1_1.setAge(2);
+        final Person person1_2 = new Person();
+        person1_2.setName("Maria");
+        person1_2.setAge(3);
         try (final ObjectOutputStream objectOutputStream =
                      new ObjectOutputStream(new FileOutputStream("serialized_person"));
              final DataOutputStream dataOutputStream =
                      new DataOutputStream(new FileOutputStream("serialized_data"))) {
-            objectOutputStream.writeObject(person);
+            objectOutputStream.writeObject(person1_0);
+            objectOutputStream.writeObject(person1_1);
+            objectOutputStream.writeObject(person1_2);
             dataOutputStream.writeBoolean(true);
             dataOutputStream.writeShort(14);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        Person person0 = null;
         Person person1 = null;
+        Person person2 = null;
         boolean boolean1 = false;
         short short1 = 0;
         try (final ObjectInputStream objectInputStream =
                      new ObjectInputStream(new FileInputStream("serialized_person"));
              final DataInputStream dataInputStream =
                      new DataInputStream(new FileInputStream("serialized_data"))) {
+            person0 = (Person) objectInputStream.readObject();
+            //objectInputStream.reset();
             person1 = (Person) objectInputStream.readObject();
+            person2 = (Person) objectInputStream.readObject();
             boolean1 = dataInputStream.readBoolean();
             short1 = dataInputStream.readShort();
         } catch (IOException | ClassNotFoundException e) {
